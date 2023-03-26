204260a56746b451c4fc23aee9579b1f59891559
chanmin Lee <dlcksals87@gmail.com>
2023-03-26 22:11:53 +0900

    make example_1.c of c programming

---
+++ C_programming/example_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int fahr, celsius = 0;
+    int lower = 0;
+    int step = 10;
+
+    printf("섭씨온도로 변경할 화씨온도를 입력하세요 (0 ~ 100 ) : ");
+    scanf("%d", fahr);
+
+    while(fahr > lower)
+    {
+        celsius = 5 * (fahr - 32) / 9;
+        printf("%d", celsius);
+        
+        fahr -= step; 
+    }
+
+    
+    return 0;
+}
