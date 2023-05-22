334f7cbaffd13b018374c0506605529cf71764a9
chanmin Lee <dlcksals87@gmail.com>
2023-05-22 22:01:37 +0900

    complete tel info inputing process from data.txt

--- C_programming/tel.c
+++ C_programming/tel.c
@@ -10,64 +10,71 @@ struct tinfo
     char memo[40];
 };
 
+tinfo tlist[MAXWORD];
 
 int main(int argc, char * argv)
 {
     FILE *fp = fopen("data.txt", "r+t");
-    char temp[MAXWORD];
-    tinfo tlist[MAXWORD];
-    char c;
-    int check_order = 0;
-    int index_temp = 0; 
-    int index_tlist = 0;     
+    char temp[MAXWORD];  
+    int check = 0;
+    char * temp_fp;
+    int index_tlist = 0;
 
-    while ((c = fgetc(fp)) != EOF)
+    while(fscanf(fp,"%s",temp) != EOF)
     {
-        if (c == '\n')
+        char * temp_fp = temp;
+        strtok(temp,":");
+        while (temp_fp != NULL)
         {
-            if (temp[index_temp] == '\0')
+            printf("%s\n", temp_fp);
+            if (check == 0)
             {
-                strcpy(tlist[index_tlist++].memo, " "); 
-                index_temp = 0;
+                strcpy(tlist[index_tlist].name, temp_fp);
+                check++;
             }
             
-            else
-            {
-                strcpy(tlist[index_tlist++].memo, temp); 
-                index_temp = 0;
-            }
-        }
-
-        else if (c == ':')
-        {
-            if (check_order == 0)
-            {
-                strcpy(tlist[index_tlist].name, temp);
-                index_temp = 0;
-                temp[index_temp]='\0';
-                check_order++;
+            else if (check == 1)
+            { 
+                strcpy(tlist[index_tlist].phone, temp_fp);
+                check++;
             }
             
-            else if (check_order == 1)
+            else if (check == 2)
             {
-                strcpy(tlist[index_tlist].phone, temp);
-                index_temp = 0;
-                temp[index_temp] = '\0';
-                check_order--;
+                strcpy(tlist[index_tlist++].memo, temp_fp);
+                check++;
             }
+            temp_fp = strtok(NULL, ":"); 
         }
-
-        else if (c == ' ' || c == '\t')
-            ;
+        if (temp_fp == NULL)
+        {
+            check == 0;
+        }
         
-        else
-        {
-            temp[index_temp++] = c;
-            temp[index_temp] = '\0';
-        }
+    }
+    temp_fp = strtok(temp, ":"); 
+    
+    printf("%s %s %s\n", tlist[0].name, tlist[0].phone, tlist[0].memo);    
+
+    argv++;
+    argc--;
+
+    if (argv[0] == "-d")
+    {
+        /* code */
     }
 
+    else if (argv[0] == "-a")
+    {
+        /* code */
+    }
+    
+    else if (argv[0] == "-l")
+    {
+        /* code */
+    }
+    
 
-
+    return 0;
 }
 
