c62bcf2c05ed57b0865ffc22599913345d3b332c
chanmin Lee <dlcksals87@gmail.com>
2023-10-12 03:22:23 +0900

    merge sort 2nd attempt (add merge_sort func)

--- Algorithm/merge_sort.c
+++ Algorithm/merge_sort.c
@@ -4,16 +4,18 @@ void merge(int * arr, int s, int m, int e)
 {
     // s : start point, m : mid point, e : end point
 
-    int n1 = m - s + 1;
-    int n2 = e - m; 
+    int n1 = m - s + 1; // left array's size
+    int n2 = e - m;     // right array's size
     int left_arr[n1+1]; 
     int right_arr[n2+1];
 
-    for (int i = 0; i < n1; i++)
+    // first for loop - copy arr values to left
+    for (int i = 0; i < n1; i++) 
     {
         left_arr[i] = arr[s + i - 1];
     }
     
+    // second for loop - copy arr values to right
     for (int j = 0; j < n2; j++)
     {
         right_arr[j] = arr[m + j];
@@ -39,4 +41,21 @@ void merge(int * arr, int s, int m, int e)
             idx_right++;
         }
     }
+}
+
+void merge_sort(int * arr, int s, int e)
+{
+    if( s < e )
+    {
+        int m = (s+e)/2;
+        merge_sort(arr, s, m);
+        merge_sort(arr, m+1, e);
+        merge(arr,s,m,e);
+    }
+}
+
+int main(void)
+{
+    int arr[5] = {1,5,3,2,4};
+    merge_sort(arr, 0, 4);
 }
