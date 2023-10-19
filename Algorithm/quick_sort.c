b9e0e4fd7bb82eebd89bab89046ad18dad798e69
chanmin Lee <dlcksals87@gmail.com>
2023-10-19 22:45:50 +0900

    quick sort

--- Algorithm/quick_sort.c
+++ Algorithm/quick_sort.c
@@ -2,39 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define ARR_SIZE 32
+int ARR_SIZE = 0;  
 
 // 이 함수 포인터로 분할 함수를 조정함. 
 int (*partition)(int *arr, int s, int e);
 int compcount = 0;
 
-int median(int num1, int num2, int num3)
+int median(int* arr, int num1, int num2, int num3)
 {
-    if (num1 >= num2 )
+    if ((arr[num1] <= arr[num2] && arr[num2] <= arr[num3]) || (arr[num3] <= arr[num2] && arr[num2] <= arr[num1]))
     {
-        if (num1 >= num3)
-        {
-            return num1;
-        }
-        
-        else
-        {
-            return num3;
-        }
-        
+        return num2;
     }
-
-    else 
+    
+    else if ((arr[num2] <= arr[num3] && arr[num3] <= arr[num1]) || (arr[num1] <= arr[num3]) && arr[num3] <= arr[num2])
     {
-        if (num2 >= num3)
-        {
-            return num2;
-        }
-        
-        else
-        {
-            return num3;
-        }
+        return num3;
+    }
+    
+    else
+    {
+        return num1;
     }
 }
 
@@ -82,54 +70,31 @@ int partition_by_random_pivot(int *arr, int s, int e)
 {
     // s : start point, e : end point
 
-    int flag = rand() % (ARR_SIZE-1) ;
-    int pivot = arr[flag];
-    int i = s - 1;
+    int flag = (rand() % (e-s)) + s ;
 
     arr_element_swap(arr, flag, e);
 
-    for (int j = s; j < e; j++)
-    {
-        if (arr[j] <= pivot)
-        {
-            i++;
-            arr_element_swap(arr, i, j);
-        }
-        compcount++;
-    }
-
-    arr_element_swap(arr, i + 1, e);
-    return i + 1;
+    return partition_by_rightmost_pivot(arr, s, e);
 }
 
 int partition_by_median_pivot(int *arr, int s, int e)
 {
     // s : start point, e : end point
 
-    int pivot = median(arr[s], arr[s+e/2], arr[e]);
-    int i = s - 1;
+    int med = median(arr, s, (s+e)/2 , e);
+    
+    arr_element_swap(arr, med, e);
 
-    for (int j = s; j < e; j++)
-    {
-        if (arr[j] <= pivot)
-        {
-            i++;
-            arr_element_swap(arr, i, j);
-        }
-        compcount++;
-    }
-
-    arr_element_swap(arr, i + 1, e);
-    return i + 1;
+    return partition_by_rightmost_pivot(arr, s, e); ;
 }
 
 int partition_by_my_pivot(int *arr, int s, int e)
 {
     int flag = rand() % (ARR_SIZE-1);
     int pivot = 0;
-    if( abs(500 - median(arr[s], arr[s+e/2], arr[e])) <= abs(500 - arr[flag]))
+    if( abs(500 - median(arr, s, (s+e)/2, e)) <= abs(500 - arr[flag]))
     {
-        pivot = median(arr[s], arr[s+e/2], arr[e]);
+        pivot = median(arr, s, (s+e)/2, e);
     }
 
     else
@@ -142,7 +107,7 @@ int partition_by_my_pivot(int *arr, int s, int e)
 
     for (int j = s; j < e; j++)
     {
-        if (arr[j] <= pivot)
+        if (arr[j] <= arr[pivot])
         {
             i++;
             arr_element_swap(arr, i, j);
@@ -167,6 +132,24 @@ void quick_sort(int *arr, int s, int e)
 
 int main()
 {
+    int scan_case = 0;
+    printf("실행할 quick sort case number를 입력하세요 : ");
+    scanf("%d", &scan_case);
+    
+    if (scan_case == 1 || scan_case == 2)
+    {
+        ARR_SIZE = 32;
+    }
+    else if(scan_case == 3)
+    {
+        ARR_SIZE = 1024;
+    }
+    else
+    {
+        printf("1,2,3 중 입력해주세요\n");
+        return 0;
+    }
+    
     // 프로그램 실행 시간 기록을 위한 변수 선언
     clock_t start_time, end_time;
     double cpu_time_used;
@@ -196,13 +179,26 @@ int main()
         large_arr[i] = rand() % 1000 + 1;
     }
 
+
+    //#######################################################################################
     // version 1
     printf("quick sort version 1 : right most element \n");
 
     int *arr_version_1;
-    arr_version_1 = array_copy(small_sorted_arr, ARR_SIZE);
 
-    // case 1-1 : sorted small size
+    if (scan_case == 1 )
+    {
+        arr_version_1 = array_copy(small_sorted_arr, ARR_SIZE);
+    }
+    else if (scan_case == 2)
+    {
+        arr_version_1 = array_copy(small_random_arr, ARR_SIZE);
+    }
+    else
+    {
+        arr_version_1 = array_copy(large_arr, ARR_SIZE);
+    }
+
     start_time = clock();
 
     partition = &partition_by_rightmost_pivot;
@@ -221,13 +217,25 @@ int main()
     cpu_time_used = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
     printf("총 실행 시간: %f 초\n", cpu_time_used);
     
+
+    //#######################################################################################
     // version 2
     printf("quick sort version 2\n");
 
     int *arr_version_2;
-    arr_version_2 = array_copy(small_sorted_arr, ARR_SIZE);
+    if (scan_case == 1 )
+    {
+        arr_version_2 = array_copy(small_sorted_arr, ARR_SIZE);
+    }
+    else if (scan_case == 2)
+    {
+        arr_version_2 = array_copy(small_random_arr, ARR_SIZE);
+    }
+    else
+    {
+        arr_version_2 = array_copy(large_arr, ARR_SIZE);
+    }
 
-    // case 1-1 : sorted small size
     start_time = clock();
 
     partition = &partition_by_random_pivot;
@@ -246,16 +254,29 @@ int main()
     cpu_time_used = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
     printf("총 실행 시간: %f 초\n", cpu_time_used);
 
+
+    //#######################################################################################
     // version 3
     printf("quick sort version 3 : median of three value \n");
 
     int *arr_version_3;
-    arr_version_3 = array_copy(small_sorted_arr, ARR_SIZE);
+    if (scan_case == 1 )
+    {
+        arr_version_3 = array_copy(small_sorted_arr, ARR_SIZE);
+    }
+    else if (scan_case == 2)
+    {
+        arr_version_3 = array_copy(small_random_arr, ARR_SIZE);
+    }
+    else
+    {
+        arr_version_3 = array_copy(large_arr, ARR_SIZE);
+    }
 
     start_time = clock();
 
     partition = &partition_by_median_pivot;
-    quick_sort(arr_version_2, 0, ARR_SIZE-1);
+    quick_sort(arr_version_3, 0, ARR_SIZE-1);
 
     printf("sorted : [ ");
     for (int i = 0; i < ARR_SIZE; i++)
@@ -270,17 +291,30 @@ int main()
     cpu_time_used = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
     printf("총 실행 시간: %f 초\n", cpu_time_used);
 
+
+    //#######################################################################################
     // version 4
     printf("quick sort version 4 : my strategy \n");
 
     int *arr_version_4;
-    arr_version_4 = array_copy(small_sorted_arr, ARR_SIZE);
+    if (scan_case == 1 )
+    {
+        arr_version_4 = array_copy(small_sorted_arr, ARR_SIZE);
+    }
+    else if (scan_case == 2)
+    {
+        arr_version_4 = array_copy(small_random_arr, ARR_SIZE);
+    }
+    else
+    {
+        arr_version_4 = array_copy(large_arr, ARR_SIZE);
+    }
 
     // case 1-1 : sorted small size
     start_time = clock();
 
     partition = &partition_by_random_pivot;
-    quick_sort(arr_version_4, 0, 31);
+    quick_sort(arr_version_4, 0, ARR_SIZE-1);
 
     printf("sorted : [ ");
     for (int i = 0; i < ARR_SIZE; i++)
